0ee4f3c0cb98667a8bc319777bb779f53dcf0ee9
Alok Shukla <alokshukla994@gmail.com>
2024-06-15 00:19:00 +0530

    feat: Implement Sales Vouchers

---
+++ ascra_billing/ascra_billing/doctype/ab_department/ab_department.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, alokshukla994@gmail.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("AB-Department", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ ascra_billing/ascra_billing/doctype/sales_receipt_voucher/sales_receipt_voucher.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2024, alokshukla994@gmail.com and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class SalesReceiptVoucher(Document):
+	def before_save(self):
+		if self.total_amt:
+			total_amt = float(self.total_amt)
+		else:
+			total_amt = 0
+		if self.tds_amount:
+			tds_amount = float(self.tds_amount)
+		else:
+			tds_amount = 0
+		self.net_bill_amt = total_amt + tds_amount
+
+		if self.item_details:
+			total_net_wt = 0
+			for item in self.item_details:
+				total_net_wt = total_net_wt + float(item.net_wt)
+				
+			total_row = self.item_details[-1]
+			if total_row.get("category") == "Total":
+				self.item_details.pop()
+				
+			self.append("item_details", {
+				"category": "Total",
+				"net_wt": total_net_wt,
+				"peices": '',
+				"melting": '',
+				"wastage": '',
+				"fine": '',
+				"other_charges": ''
+			})
