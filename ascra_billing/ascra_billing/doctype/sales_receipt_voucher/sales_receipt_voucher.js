27d617ce20494879505bf3c4cbcc0a94d075d745
root <root@LAPTOP-BS7FE5EC>
2024-07-01 22:59:23 +0530

    calculation changes

--- ascra_billing/ascra_billing/doctype/sales_receipt_voucher/sales_receipt_voucher.js
+++ ascra_billing/ascra_billing/doctype/sales_receipt_voucher/sales_receipt_voucher.js
@@ -1,8 +1,136 @@
 // Copyright (c) 2024, alokshukla994@gmail.com and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Sales Receipt Voucher", {
-// 	refresh(frm) {
+frappe.ui.form.on('Sales Receipt Voucher', {
+	refresh(frm) {
+        setStatusPill(frm)
+	    setQueryFilter(frm);
+        if(frm.doc.docstatus == 1){
+            addSIButton(frm);
+        }
+        else{
+            addActionButton(frm);
+        }
+	},
+	
+	account_code(frm) {
+        // On change of Account Code Set Query FIlter Again
+	    setQueryFilter(frm);
+	},
+    on_update(frm){
+        console.log("Hey")
+    }
 
-// 	},
-// });
+})
+
+function setStatusPill(frm){
+        if(document.getElementById('doc-status')){
+            document.getElementById('doc-status').remove();
+        }
+        let title_element = $(`h3[title="${frm.doc.name}"]`).parent();
+        title_element = title_element[0];
+        const pillspan = document.createElement('span');
+        pillspan.setAttribute('class', 'indicator-pill whitespace-nowrap');
+
+        let color = 'grey';  // Default color is grey
+        let status = 'Pending'
+        if (frm.doc.billing_status === 'approve') {
+            color = 'green';
+            status = 'Approved'
+        } else if (frm.doc.billing_status === 'reject') {
+            color = 'red';
+            status = 'Rejected'
+        }
+
+        pillspan.classList.add(
+            color
+        );
+        pillspan.setAttribute('id', 'doc-status');
+        pillspan.style.marginLeft = 'var(--margin-sm)';
+        const textspan = document.createElement('span');
+        textspan.innerHTML = status;
+        pillspan.appendChild(textspan);
+
+        title_element.appendChild(pillspan);
+  }
+
+
+function setQueryFilter(frm){
+    frappe.call({
+        method: "ascra_billing.ascra_billing.doctype.sales_receipt_voucher.sales_receipt_voucher.get_address_by_account_code",
+         args: {
+             account_code : frm.doc.account_code
+         },
+         callback: (r)=> {
+         var address = r.message;
+         frm.set_query("sub_account", function() {
+             return {
+                 filters: {
+                         name: ["in", address],
+                 },
+             };
+         });
+
+         frm.set_query("shipping_to_address", function() {
+            return {
+                filters: {
+                        name: ["in", address],
+                },
+            };
+        });
+
+
+         }
+     });
+}
+
+function addSIButton(frm){
+    frm.add_custom_button(__("Create Sales Invoice"), () =>
+        frappe.model.open_mapped_doc({
+			method: "ascra_billing.ascra_billing.doctype.sales_receipt_voucher.sales_receipt_voucher.make_sales_invoice",
+			frm: frm,
+		}),
+    );
+}
+
+function addActionButton(frm){
+    frm.page.add_action_item(__("Approve"), function() {
+        frm.doc.billing_status = 'approve'
+        // Submit Doc
+        refresh_field("billing_status")
+        frm.page.actions.find('[data-label="Reject"],[data-label="Approve"]').parent().parent().remove()
+        frm.save("Submit")
+
+
+        frappe.model.open_mapped_doc({
+			method: "ascra_billing.ascra_billing.doctype.sales_receipt_voucher.sales_receipt_voucher.make_sales_invoice",
+			frm: frm,
+		})
+
+    });
+
+    frm.page.add_action_item(__("Reject"), function() {
+        if(frm.doc.docstatus == 1){
+            frappe.throw("Document Already Approved")
+        }
+        frappe.msgprint("Rejected");
+        // frm.doc.billing_status = 'reject'
+        // frm.set_value("billing_status", "reject")
+
+        frappe.call({
+            method: "ascra_billing.ascra_billing.doctype.sales_receipt_voucher.sales_receipt_voucher.set_status",
+             args: {
+                docname: frm.doc.name,
+                status : 'reject'
+             },
+             callback: (r)=> {
+                
+                window.location.reload();
+    
+             }
+         });
+
+
+        refresh_field("billing_status")
+    });
+}
--- ascra_billing/ascra_billing/doctype/sales_receipt_voucher/sales_receipt_voucher.py
+++ ascra_billing/ascra_billing/doctype/sales_receipt_voucher/sales_receipt_voucher.py
@@ -6,7 +6,20 @@ from frappe.model.document import Document
 
 
 class SalesReceiptVoucher(Document):
+	def on_update(self):
+		if self.workflow_state == "Approved":
+			send_status(self, "Approved")
+		elif self.workflow_state == "Rejected":
+			send_status(self, "Rejected")
+
 	def before_save(self):
+		# Fetch Customer
+		self.customer = frappe.db.get_value("Customer", {"custom_account_code": self.account_code})
+		amount_tcs_tds = 0
+		taxable_value = 0
+		total_net_wt = 0
+		total_pcs = 0
+		total_fine = 0
 		if self.total_amt:
 			total_amt = float(self.total_amt)
 		else:
@@ -15,23 +28,255 @@ class SalesReceiptVoucher(Document):
 			tds_amount = float(self.tds_amount)
 		else:
 			tds_amount = 0
-		self.net_bill_amt = total_amt + tds_amount
+		# self.net_bill_amt = total_amt + tds_amount
 
 		if self.item_details:
 			total_net_wt = 0
 			for item in self.item_details:
-				total_net_wt = total_net_wt + float(item.net_wt)
-				
+				total_net_wt += float(item.net_wt)
+				total_pcs += float(item.pieces)
+				total_fine += float(item.fine)
+				print(total_net_wt)
+
 			total_row = self.item_details[-1]
 			if total_row.get("category") == "Total":
 				self.item_details.pop()
-				
+			
+			total_net_wt = 1000
+
 			self.append("item_details", {
 				"category": "Total",
 				"net_wt": total_net_wt,
-				"peices": '',
+				"pieces": float(total_pcs),
 				"melting": '',
 				"wastage": '',
-				"fine": '',
+				"fine": total_fine,
 				"other_charges": ''
 			})
+
+		if self.tcs_amount !=0 and self.purc_sale_type == "":
+			amount_tcs_tds = float(self.fine_amount or 0) + float(self.total_other_charge or 0) + float(self.gst_amount or 0) + float(self.discount_amount or 0) + float(self.total_logistic_amount or 0) + float(self.total_hallmark_amount or 0)
+			print("amount_tcs_tds1")
+			print(amount_tcs_tds)
+		else :
+			amount_tcs_tds = float(self.total_amt or 0)			
+
+		# self.amount_tcs_tds = round(amount_tcs_tds)
+		#amount_tcs_tds = self.total_amt
+		
+		print("gst name")
+		print(self.gst_name)
+
+		if self.gst_name != "" and self.purc_sale_type == "":
+			if (self.gst_name).lower() == 'sale bill':
+				amount_tcs_tds = float(amount_tcs_tds)
+				print("amount_tcs_tds")
+				print(amount_tcs_tds)
+				rate_per_gram = ((amount_tcs_tds/103)*100)/total_net_wt
+				print("rate_per_gram1")
+				print(rate_per_gram)
+			elif (self.gst_name).lower() == 'labour bill':
+				amount_tcs_tds = float(amount_tcs_tds)
+				rate_per_gram = ((amount_tcs_tds/105)*100)/total_net_wt
+			else:
+				rate_per_gram = float(amount_tcs_tds/total_net_wt or 0)
+		else :
+			rate_per_gram = float(amount_tcs_tds/total_net_wt)
+
+
+		print("rate per gram")
+		print(rate_per_gram)
+
+		billing_gold_rate = 0
+		self.billing_gold_rate = float(rate_per_gram or 0)
+		taxable_value = float(total_net_wt) * float(rate_per_gram)
+		self.total_fine_amount = round(taxable_value)
+
+@frappe.whitelist()
+def submit_doc(doc):
+	doc = json.loads(doc)
+	doc.submit()
+
+# ascra_billing.ascra_billing.doctype.sales_issue_voucher.sales_issue_voucher.get_address_by_account_code
+@frappe.whitelist()
+def get_address_by_account_code():
+	acc_code = frappe.form_dict.account_code
+	if not frappe.form_dict.account_code:
+		frappe.response['message'] = []
+		
+	cust = frappe.db.get_value("Customer", {"custom_account_code": acc_code})
+
+	if not cust:
+		frappe.response['message'] = []
+	else:
+		add_filter = [["Dynamic Link","link_name","=",cust]]
+		address = [i.get("name") for i in frappe.get_all("Address", add_filter, ['name'])]
+		frappe.response['message'] = address
+
+
+@frappe.whitelist()
+def make_sales_invoice(source_name, target_doc=None):
+	from frappe.model.mapper import get_mapped_doc
+
+	def post_process(source, target):
+		target.posting_date = frappe.utils.now_datetime()
+
+		source.item_details.pop() # Remove the last Total Element
+		total_net_wt = 0
+		total_pcs = 0
+		total_fine = 0
+
+		if source.generate_bill_type == 'items':
+			for item in source.item_details:
+				target.append("items", {
+					"custom_department": item.get("department_name"),
+					"rate": source.billing_gold_rate,
+					"qty": item.net_wt,
+					"custom_pieces": item.pieces
+				})
+		else:
+			department = source.item_details[0].get("department_name")
+			for item in source.item_details:
+				total_net_wt += float(item.net_wt)
+				total_pcs += float(item.pieces)
+
+			target.append("items", {
+					"custom_department": department,
+					"qty": total_net_wt,
+					"custom_pieces" : total_pcs,
+					"rate": source.billing_gold_rate
+				})
+
+		if source.display_making_charges:
+			item_details = get_item_details(
+				company=get_default_company(),
+				item_code="MakingCharges"
+			).get("message")
+			frappe.logger("utils").exception(item_details)
+			target.append("items", {
+					"qty": 1,
+					"custom_pieces" : total_pcs,
+					"rate": source.making_charges,
+					"custom_item":"MakingCharges",
+					"uom" : item_details.get("stock_uom"),
+                    "income_account" : item_details.get("income_account"),
+                    "item_name" : item_details.get("item_name"),
+                    "cost_center" : item_details.get("cost_center"),
+					"gst_hsn_code": "12345678"
+				})
+
+	doclist = get_mapped_doc(
+		"Sales Receipt Voucher",
+		source_name,
+		{
+			"Sales Receipt Voucher": {"doctype": "Sales Invoice",
+			"field_map": {
+					"billing_gold_rate": "custom_gold_rate",
+					"gold_rate_with_gst": "custom_gold_rate__with_gst",
+					"sub_account":"customer_address",
+					"shipping_to_address": "shipping_address_name"
+				}},
+		},
+		target_doc,
+		post_process,
+	)
+
+	return doclist
+
+def get_default_company():
+    user = frappe.session.user
+    default_company = frappe.defaults.get_user_default("Company", user)
+    return default_company
+
+@frappe.whitelist()
+def get_item_details(company=None, item_code = None):
+	company = frappe.form_dict.company or company
+	item_code = frappe.form_dict.item_code or item_code
+	item_details = frappe.get_value("Item", item_code, "*", as_dict=True)
+	frappe.logger("utils").exception(item_details)
+	company = frappe.get_value("Company", company, "*", as_dict=True)
+	item_details_dict = {
+		"stock_uom": item_details.get("stock_uom"),
+		"item_name": item_details.get("item_name"),
+		"income_account": company.get("default_income_account"),
+		"cost_center": company.get("cost_center")
+	}
+	frappe.response['message'] = item_details_dict
+
+	return {
+		"message": item_details_dict
+	}
+
+
+def send_status(self, status):
+	try:
+		settings_doc = frappe.get_single("Ascra Billing Settings")
+		url = settings_doc.api_url
+		if not url:
+			frappe.logger("workflow_status_log").exception(f"Api URL Not Configured")
+			return
+		billing_status = 1 if status == "Approved" else 2
+		frappe.logger("workflow_status_log").exception(billing_status)
+		payload = {
+			"id": self.name,
+			"billing_notes": self.billing_notes,
+			"billing_status": billing_status,
+			"billing_status_reason_remark": "",
+			"billing_status_feedback_remark": "", # TODO: Add Fields
+			"billing_status_approved_reject_by": "", # TODO: Add Fields
+			"voucher_billing_dept_cat_type": self.voucher_billing_dept_cat_type,
+			"generate_bill_type": self.generate_bill_type,
+			"sub_account_name": self.sub_account,
+			"sub_account_id": 0, # TODO: Fetch this Field Via UI
+			"display_making_charges": self.display_making_charges,
+			"godown_bill": self.godown_bill
+		}
+		files=[
+
+		]
+		headers = {}
+
+		response = requests.request("POST", url, headers=headers, data=payload, files=files)
+
+		frappe.logger("workflow_status_log").exception(f"Id : {self.id}, {response.json()}")
+	except Exception as e:
+		frappe.logger("workflow_status_log").exception(f"Id : {self.id}, {e}")
+
+# @frappe.whitelist()
+# def set_status(docname, status):
+# 	frappe.db.set_value("Sales Issue Voucher", docname, "billing_status", status)
+# 	frappe.db.commit()
+
+# 	def send_status(self, status):
+# 		try:
+# 			settings_doc = frappe.get_single("Ascra Billing Settings")
+# 			url = settings_doc.api_url
+# 			if not url:
+# 				frappe.logger("workflow_status_log").exception(f"Api URL Not Configured")
+# 				return
+# 			billing_status = 1 if status == "Approved" else 2
+# 			frappe.logger("workflow_status_log").exception(billing_status)
+# 			payload = {
+# 				"id": self.name,
+# 				"billing_notes": self.billing_notes,
+# 				"billing_status": billing_status,
+# 				"billing_status_reason_remark": "",
+# 				"billing_status_feedback_remark": "", # TODO: Add Fields
+# 				"billing_status_approved_reject_by": "", # TODO: Add Fields
+# 				"voucher_billing_dept_cat_type": self.voucher_billing_dept_cat_type,
+# 				"generate_bill_type": self.generate_bill_type,
+# 				"sub_account_name": self.sub_account,
+# 				"sub_account_id": 0, # TODO: Fetch this Field Via UI
+# 				"display_making_charges": self.display_making_charges,
+# 				"godown_bill": self.godown_bill
+# 			}
+# 			files=[
+
+# 			]
+# 			headers = {}
+
+# 			response = requests.request("POST", url, headers=headers, data=payload, files=files)
+
+# 			frappe.logger("workflow_status_log").exception(f"Id : {self.id}, {response.json()}")
+# 		except Exception as e:
+# 			frappe.logger("workflow_status_log").exception(f"Id : {self.id}, {e}")
